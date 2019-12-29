a5d5418a70455e01f23b1e12087e96922f344c67
Simon Li <orpheus+devel@gmail.com>
2019-12-29 21:45:02 +0000

    Add long description

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='jupyter-pyfilesystem',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/manics/jupyter-pyfilesystem',
     author='Simon Li',
     license='BSD 3-Clause',
     description='Jupyter Notebook PyFilesystem Contents Manager',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     install_requires=[
         'notebook',
